3864650a592764ecd80273901f49642992985041
Unknown <arkadiuszduliban@gmail.com>
2017-11-17 18:42:49 +0100

    Changes in CORS

--- PlugAPI/Cors/AllowCross.cs
+++ PlugAPI/Cors/AllowCross.cs
@@ -8,13 +8,14 @@ namespace PlugAPI.Cors
 {
     public class AllowCross : ActionFilterAttribute
 {
-    public override void OnActionExecuting(ActionExecutingContext filterContext)
-    {
-        filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "http://localhost:8082/");
-        filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Headers", "*");
-        filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Credentials", "true");
+    //public override void OnActionExecuting(ActionExecutingContext filterContext)
+    //{
+    //    filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "http://localhost:8082/");
+    //    filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Headers", "*");
+    //        filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Methods:", "*");
+    //        filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Credentials", "true");
 
-        base.OnActionExecuting(filterContext);
-    }
+    //    base.OnActionExecuting(filterContext);
+    //}
 }
 }
