dfbb647fbfc5dd6ee05625fcc46568fc0cedc115
Unknown <arkadiuszduliban@gmail.com>
2017-11-25 13:34:56 +0100

    Schedules added by webapi will run properly

--- PlugAPI/Controllers/ScheduleController.cs
+++ PlugAPI/Controllers/ScheduleController.cs
@@ -106,6 +106,9 @@ namespace PlugAPI.Controllers
                 }
             }
 
+            UpdateSchedule(ref msgToSend);
+
+
 
             XDocument msgReceived;
             try
@@ -120,6 +123,101 @@ namespace PlugAPI.Controllers
             return Get();
         }
 
+
+        private void UpdateSchedule(ref XDocument msg)
+        {
+            for (int i=0; i<=6; i++) // for each day of week
+            {
+                var value = msg.Descendants("Device.System.Power.Schedule." + i + ".List").First().Value;
+                if (value.Length > 0)
+                {
+                    string[] encodedHours = value.Split('-');
+                    var scheduleList = new List<Schedule>();
+                    foreach(var h in encodedHours)
+                    {
+                        if (!String.IsNullOrEmpty(h))
+                            scheduleList.Add(DecodeHelper.DecodeString(i, h));
+                    }
+
+                    msg.Descendants("Device.System.Power.Schedule." + i).First().Value = PreparePowerSchedule(scheduleList);
+                }
+            }
+        }
+
+        private string PreparePowerSchedule(IEnumerable<Schedule> entriesToSchedule)
+        {
+            int[] minuteSchedule = InitMinuteSchedule(); // an array containing one element for each minute of a day
+
+
+            foreach (Schedule entry in entriesToSchedule)
+            {
+                // filling in scheduled minutes for each scheduled entry
+                FillMinuteSchedule(entry, ref minuteSchedule);
+            }
+
+            //Transfer it to a 360 elements based hex schedule
+
+            char[] hexSchedule = InitHexSchedule();
+
+            int schedulePosition = 0;
+            for (int quadrupletStart = 0; quadrupletStart < 1440; quadrupletStart += 4)
+            {
+                hexSchedule[schedulePosition] = QuadrupletToHex(minuteSchedule, quadrupletStart);
+                schedulePosition++;
+            }
+
+            return new string(hexSchedule); // string with length 360, each position contains a value between '0' and 'F'
+        }
+
+        private static int[] InitMinuteSchedule()
+        {
+            const int MinutesOfADay = 1440;
+            int[] scheduleTemplate = new int[MinutesOfADay];
+            for (int i = 0; i < MinutesOfADay; i++)
+            {
+                scheduleTemplate[i] = 0;
+            }
+            return scheduleTemplate;
+        }
+
+        private static char[] InitHexSchedule()
+        {
+            char[] scheduleTemplate = new char[360];
+            for (int i = 0; i < 360; i++)
+            {
+                scheduleTemplate[i] = '0';
+            }
+            return scheduleTemplate;
+        }
+
+        static void FillMinuteSchedule(Schedule entry, ref int[] minuteSchedule)
+        {
+            var hourStart = entry.HourStart.Split(':');
+            var hourEnd = entry.HourEnd.Split(':');
+            FillMinuteSchedule(int.Parse(hourStart[0]), int.Parse(hourStart[1]),
+                              int.Parse(hourEnd[0]), int.Parse(hourEnd[1]), ref minuteSchedule);
+
+        }
+
+        static void FillMinuteSchedule(int fromHour, int fromMinute, int toHour, int toMinute, ref int[] minuteSchedule)
+        {
+            int begin = fromHour * 60 + fromMinute;
+            int end = toHour * 60 + toMinute;
+            for (int pos = begin; pos < end; pos++)
+            {
+                minuteSchedule[pos] = 1;
+            }
+        }
+
+        private static char QuadrupletToHex(int[] minuteSchedule, int quadrupletBaseAdress)
+        {
+            int quadrupletSum = 0;
+            for (int position = quadrupletBaseAdress; position < (quadrupletBaseAdress + 4); position++)
+            {
+                quadrupletSum = quadrupletSum * 2 + minuteSchedule[position];
+            }
+            return quadrupletSum.ToString("X")[0];
+        }
 
 
         /// <summary>
